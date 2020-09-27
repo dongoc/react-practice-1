0e69627a97c5aa1b64d883fdf263c393056eec7f
dongoc <dongoc21hj@gmail.com>
2020-09-27 17:47:57 +0900

    3-16: Showing Custom Children

--- src/ApprovalCard.js
+++ src/ApprovalCard.js
@@ -1,10 +1,12 @@
 import React from 'react'
 
-const ApprovalCard = () => {
+const ApprovalCard = (props) => {
+  console.log(props) // {children: {…}}
+  console.log(props.children) // {{$$typeof: Symbol(react.element), key: null, ref: null, props: {…}, type: ƒ, …}
   return (
     <div className="ui card">
       <div className="content">
-        Are you sure?
+        {props.children}
       </div>
       <div className="extra content">
         <div className="ui basic green button">Approve</div>
