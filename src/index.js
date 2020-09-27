c677b716d5d37f504124e67ef063cc87c77d3739
dongoc <dongoc21hj@gmail.com>
2020-09-27 16:15:21 +0900

    2-11: Displaying Content with Functional Components

--- src/index.js
+++ src/index.js
@@ -2,8 +2,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-
-
 // Create a React component
+const App = () => {
+  return <div>Hi there!</div>;
+}
 
 // Take the react component and show it on the screen
+ReactDOM.render(
+  <App/>,
+  document.querySelector('#root')
+)
--- src/index.md
+++ src/index.md
@@ -11,3 +11,9 @@ import ReactDOM from 'react-dom';
 const React = require('react');
 ```
 ![](../note-images/2-10-2-import.png)
+
+
+## 2. Component?
+1. show HTML using JSX
+2. handle feedback using Event Handler
+![](../note-images/2-11-1-component.png)
